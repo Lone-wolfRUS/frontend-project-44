9240c621425af130181a8f2534bb2137eda56007
Lone-wolfRUS <wolf93490@gmail.com>
2026-04-19 13:47:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import readlineSync from 'readline-sync';
+import headlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
 const runGameEngine = (description, generateRound) => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
+  const name = headlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(description);
 
   for (let i = 0; i < roundsCount; i += 1) {
     const [question, correctAnswer] = generateRound();
     console.log(`Question: ${question}`);
-    const answer = readlineSync.question('Your answer: ');
+    const answer = headlineSync.question('Your answer: ');
 
     if (answer !== correctAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`'${answer}' is wrong answer; . Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
