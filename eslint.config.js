284922d3efa48a0a2a02802974518401efd3260a
Lone-wolfRUS <wolf93490@gmail.com>
2026-05-18 15:38:01 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
-
+import stylistic from '@stylistic/eslint-plugin'
+ 
 export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
       'semi': ['error', 'never'],
       'no-console': 'off',
+      '@stylistic/no-trailing-spaces': 'error',
     },
   },
 ]
